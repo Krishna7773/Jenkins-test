6f34d62391359c1e495e75b41a3a6e194866233a
Krishna Panwar <krishnapanwar201@gmail.com>
2024-06-26 15:16:06 +0530

    one change for test

--- Fact.java
+++ Fact.java
@@ -9,6 +9,7 @@ class Fact{
   int i,fact=1;  
   int number=4;//It is the number to calculate factorial    
   fact = factorial(number);   
-  System.out.println("Factorial of "+number+" is: "+fact);    
+  System.out.println("Factorial of "+number+" is: "+fact);
+  System.out.println("jenkins-test");    
  }  
 }  
